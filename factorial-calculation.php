30aced8b909a04ea5303674485a77ab5df80de70
Billy McDonald <bmcdona2@cord.edu>
2014-04-29 22:07:16 -0500

    Factorial example

---
+++ factorial-calculation.php
@@ -0,0 +1,22 @@
+<? /*
+* @description The following is a PHP example of the famous "merge sort" algorithm.
+* @author Billy McDonald <billyjmcdonald@gmail.com>
+*/
+
+$number = 10;
+
+/**
+ * @param $n Number to compute factorial of
+ * @return mixed result of computing factorial of $n
+ */
+function factor($n){
+    // check if base case has been reached, if so return an begin
+    // return method results to the caller
+    if($n  <= 1){
+        return $n;
+    } else {
+        // multiple n by the result of n - 1
+        return ($n * factor($n -1 ));
+    }
+
+}
